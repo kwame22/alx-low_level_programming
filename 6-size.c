d189b564fe799dc0a5d435066e4fad0f82a678ce
Felix <kwaame22@gmail.com>
2023-03-16 16:22:42 -0700

    add files 0 to 6

---
+++ 6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
