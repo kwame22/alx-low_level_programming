c45d1f444740d1cc86175bfed3b15b4080d230a3
Felix <kwaame22@gmail.com>
2023-03-16 16:57:05 -0700

    add 5 with betty style comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void)
+
+/**
+ * Main prints the string the function
+ * Description: Using the main function
+ * this program prints the string 
+ * Return 0
+ */
+
+int main(void)	
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
