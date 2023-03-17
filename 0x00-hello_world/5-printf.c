703f6469f654f879c16f2670efa647e48800f7e2
Felix <kwaame22@gmail.com>
2023-03-16 17:15:41 -0700

    add 5 modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,12 +4,13 @@
  * Main - prints the string in the function
  * You are not allowed to use the function puts
  * Your program should compile without warning when using the -Wall gcc option
- * Description: The main function prints the string it receives
- * Return 0
+ * Description: using the main function
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
