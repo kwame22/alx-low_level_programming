014274b063152b7c8bbe19879a370c0af7fd8464
Felix <kwaame22@gmail.com>
2023-03-16 17:09:27 -0700

    add 5-printf.c pesky betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * Main prints the string the function
- * Description: Using the main function
- * this program prints the string 
+ * Main - prints the string in the function
+ * You are not allowed to use the function puts
+ * Your program should compile without warning when using the -Wall gcc option
+ * Description: The main function prints the string it receives
  * Return 0
  */
 
-int main(void)	
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
