1658bc6c8c30ef41b2521d9a121d2745f0d2988d
Felix <kwaame22@gmail.com>
2023-03-18 07:19:11 -0700

    add 1-last_digit.c betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - prints the last digit of output and compares the value
- * 
+ *
  * Description: The program prints the last digit of output
  * and compares the last digit to 5 and 0
  * Return: 0
@@ -20,8 +20,8 @@ int main(void)
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n,last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n,last_digit);
 	else
-		printf("Last digit of %d is %d and less than 6 and not zero\n", n, last_digit);
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n,last_digit);
 	return (0);
 }
