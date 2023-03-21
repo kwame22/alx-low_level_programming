150e1a13457d6efb8463d57394dd9ea91876a72c
Felix <kwaame22@gmail.com>
2023-03-21 16:18:24 -0700

    add 0 and main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar to output
@@ -9,6 +9,25 @@
  */
 int main(void)
 {
-	printf("_putchar \n");
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
