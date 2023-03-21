c555585eb99808d03157082852b19ae041ad3054
Felix <kwaame22@gmail.com>
2023-03-21 07:57:12 -0700

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar to output
+ *
+ * Description: The program prints to
+ * output the word _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A repo on functions and nested loops ALX
