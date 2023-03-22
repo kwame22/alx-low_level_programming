0f19b59725346927e3d475bda93f9b7311ce4124
Felix <kwaame22@gmail.com>
2023-03-22 14:55:35 -0700

    add 6 another parameter correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * _abs - prints absolute value
- * @int: function accepts an int value
+ * @i: function accepts an int value 'i'
  *
  * Description: prints the absolute value
  * of an integer from input
  * Return: 0
  */
-int _abs(int)
+int _abs(int i)
 {
 	int abs_value;
 
-	if (int < 0)
+	if (i < 0)
 	{
-		abs_value = int * -1;
+		abs_value = i * -1;
 		_putchar(abs_value + '0');
 	}
 	else
 	{
-		abs_value = int;
+		abs_value = i;
 		_putchar(abs_value + '0');
 	}
 	return (0);
