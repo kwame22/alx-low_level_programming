e98ec7a7a6e52bac74acd5d2d2b001cee85bffa5
Felix <kwaame22@gmail.com>
2023-03-22 14:51:57 -0700

    add 6 parameter correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,17 +9,16 @@
  */
 int _abs(int)
 {
-	int i;
 	int abs_value;
 
-	if (i < 0)
+	if (int < 0)
 	{
-		abs_value = i * -1;
+		abs_value = int * -1;
 		_putchar(abs_value + '0');
 	}
 	else
 	{
-		abs_value = i;
+		abs_value = int;
 		_putchar(abs_value + '0');
 	}
 	return (0);
